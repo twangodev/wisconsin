02622a8d2655a1338f4fc5f6898ce650e6124e0b
James Ding <jamesding365@gmail.com>
2025-09-23 23:15:38 -0500

    Update Graph component configuration to set localGraph depth

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        depth: 2,
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
